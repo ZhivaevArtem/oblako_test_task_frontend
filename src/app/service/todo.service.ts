1d049445eb403e968d544b630bb4eaf09909ef59
Zhivaev Artem <zhivaev993@gmail.com>
2020-06-28 23:35:32 +0300

    todo task sort

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -3,13 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Category } from '../model/category';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  private targetUrl: string = environment.restUrl + '/projects';
+  private targetUrl: string = `${environment.restUrl}/projects`;
   private createTodoUrl: string = `${environment.restUrl}/todos`;
 
   constructor(
@@ -24,7 +25,14 @@ export class TodoService {
   }
 
   public getTodos(): Observable<Category[]> {
-    return this.httpClient.get<Category[]>(this.targetUrl);
+    return this.httpClient.get<Category[]>(this.targetUrl).pipe(
+      map(categories => {
+        for (let cat of categories) {
+          cat.todos = cat.todos.sort((todo1, todo2) => todo1.id - todo2.id);
+	}
+        return categories.sort((cat1, cat2) => cat1.id - cat2.id);
+      })
+    );
   }
 
   public patch(categoryId, todoId, isCompleted): Observable<any> {
